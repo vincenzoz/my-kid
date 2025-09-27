c8b9f603df3e29a9a66e074d84d8c1b834e4851a
vincenzoz <16963947+vincenzoz@users.noreply.github.com>
2025-09-27 16:22:25 +0200

    test database

---
+++ backend/src/common/database-utils.js
@@ -0,0 +1,26 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export class DbService {
+
+    static async getData(tableName) {
+        console.debug('SUPABASE_URL: ' + SUPABASE_URL);
+        console.debug('tableName: ' + tableName);
+        console.log('Runtime detection:', process.version);
+
+        const {data, error, status} = await supabase.from('school_communications').select('*');
+
+        if (error) {
+            console.error('Errore nella query:', error.message);
+        } else {
+            console.table(data);
+        }
+        console.log('status', status, 'error', error, 'data', data);
+
+        return data;
+    }
+}
