67d0c01c90ebd6b4ba32099bddce187e2db8a992
vincenzoz <16963947+vincenzoz@users.noreply.github.com>
2025-10-05 00:34:11 +0200

    fixed get communication by id

--- api/school/communications/[id].js
+++ api/school/communications/[id].js
@@ -2,18 +2,26 @@ import {
     getSchoolCommunicationById
 } from "../../../backend/src/services/school.service.js";
 
-export default async function handler(req, res) {
+export default async function handlerGetById(req, res) {
     console.log('AAAA')
     if(req.method === "GET") {
         console.log("[API] GET - /school/communication/[ID]");
         try {
-            const {id} = req.params;
-            const data = await getSchoolCommunicationById(id);
+            console.log('before');
+            const id = req.query.id;
+            console.log('after: ' + id);
+            if (!id || isNaN(Number(id))) {
+                return res.status(400).json({ error: "Invalid ID" });
+            }
+            const data = await getSchoolCommunicationById(Number(id));
+            if (!data) {
+                return res.status(404).json({ error: "Data not found" });
+            }
             return res.status(200).json(data);
         } catch (err) {
-            res.status(500).json({ error: error.message });
+            return res.status(500).json({ error: err.message });
         }
     }
-    res.setHeader('Allow', ['GET', 'POST'])
-    res.status(405).end(`Method ${req.method} Not Allowed`)
+    res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`)
 }
